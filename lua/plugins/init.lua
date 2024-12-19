68e357cbfcfa18874534570950b68e23a6703cfa
Nekketsu <nekketsu@gmail.com>
2024-12-19 12:04:39 +0100

    Update config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -501,13 +501,12 @@ return {
 
     {
         "chrisgrieser/nvim-various-textobjs",
-        opts = {
+        event = "VeryLazy",
+        opts = { 
+            keymaps = {
+                useDefaults = true 
+            }
         },
-        config = function()
-            require("various-textobjs").setup({
-                useDefaultKeymaps = true,
-            })
-        end
     },
 
     {
@@ -614,35 +613,35 @@ return {
         config = true
     },
 
-    {
-        "sphamba/smear-cursor.nvim",
-        opts = {},
-    },
-
-    {
-        "karb94/neoscroll.nvim",
-        config = function ()
-            local neoscroll = require('neoscroll')
-            neoscroll.setup({
-                -- Default easing function used in any animation where
-                -- the `easing` argument has not been explicitly supplied
-                easing = "quadratic"
-            })
-            local keymap = {
-                -- Use the "sine" easing function
-                ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 100; easing = 'sine' }) end;
-                ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 100; easing = 'sine' }) end;
-                -- Use the "circular" easing function
-                ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 100; easing = 'circular' }) end;
-                ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 100; easing = 'circular' }) end;
-                -- When no value is passed the `easing` option supplied in `setup()` is used
-                ["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor=false; duration = 100 }) end;
-                ["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor=false; duration = 100 }) end;
-            }
-            local modes = { 'n', 'v', 'x' }
-            for key, func in pairs(keymap) do
-                vim.keymap.set(modes, key, func)
-            end
-        end
-    }
+    -- {
+    --     "sphamba/smear-cursor.nvim",
+    --     opts = {},
+    -- },
+    --
+    -- {
+    --     "karb94/neoscroll.nvim",
+    --     config = function ()
+    --         local neoscroll = require('neoscroll')
+    --         neoscroll.setup({
+    --             -- Default easing function used in any animation where
+    --             -- the `easing` argument has not been explicitly supplied
+    --             easing = "quadratic"
+    --         })
+    --         local keymap = {
+    --             -- Use the "sine" easing function
+    --             ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 100; easing = 'sine' }) end;
+    --             ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 100; easing = 'sine' }) end;
+    --             -- Use the "circular" easing function
+    --             ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 100; easing = 'circular' }) end;
+    --             ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 100; easing = 'circular' }) end;
+    --             -- When no value is passed the `easing` option supplied in `setup()` is used
+    --             ["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor=false; duration = 100 }) end;
+    --             ["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor=false; duration = 100 }) end;
+    --         }
+    --         local modes = { 'n', 'v', 'x' }
+    --         for key, func in pairs(keymap) do
+    --             vim.keymap.set(modes, key, func)
+    --         end
+    --     end
+    -- }
 }
