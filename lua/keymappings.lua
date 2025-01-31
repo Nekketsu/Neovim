9afb649fbed24e45cfc1a73015e1eb75f96b505b
Nekketsu <nekketsu@gmail.com>
2025-01-31 12:38:59 +0100

    Update diagnostics configuration

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -6,9 +6,9 @@ vim.keymap.set('n', '<space>Y', function() require("pickers.spectre").toggle() e
 
 vim.keymap.set("n", "yoe", function()
     if vim.diagnostic.config().virtual_lines then
-        vim.diagnostic.config({ virtual_lines = false, virtual_text = true })
+        vim.diagnostic.config({ virtual_lines = false, virtual_text = true, severity = { min = vim.diagnostic.severity.WARN} })
     else
-        vim.diagnostic.config({ virtual_lines = true, virtual_text = false })
+        vim.diagnostic.config({ virtual_lines = true, virtual_text = false, severity = { min = vim.diagnostic.severity.WARN} })
     end
 end)
 vim.keymap.set("n", "[oe", function() vim.diagnostic.config({ virtual_lines = true, virtual_text = false }) end)
